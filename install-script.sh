54282554f1d38fc50d0c9f4eeb9f705dd1e62ec4
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2025-03-01 16:37:15 +0800

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -12,8 +12,8 @@ echo -e "[SETUP] Create folders"
 mkdir -p logs tmp www
 
 # Clone the default repository into a temporary directory
-echo "[Git] Cloning default repository 'https://github.com/Ym0T/pterodactyl-nginx-egg' into temporary directory."
-git clone https://github.com/Ym0T/pterodactyl-nginx-egg /mnt/server/gtemp > /dev/null 2>&1 && echo "[Git] Repository cloned successfully." || { echo "[Git] Error: Default repository clone failed."; exit 21; }
+echo "[Git] Cloning default repository 'https://github.com/Xhost-tw/pterodactyl-nginx-egg' into temporary directory."
+git clone https://github.com/Xhost-tw/pterodactyl-nginx-egg /mnt/server/gtemp > /dev/null 2>&1 && echo "[Git] Repository cloned successfully." || { echo "[Git] Error: Default repository clone failed."; exit 21; }
 
 # Copy the www folder and files from the temporary repository to the target directory
 echo "[Git] Copying folder and files from default repository."
